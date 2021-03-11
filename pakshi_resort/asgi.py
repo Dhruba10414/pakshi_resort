174a551b8c4bc83c08dd61f15b9083ac2f02fef6
Sudipto <sudipto00003@gmail.com>
2021-03-11 12:13:45 +0600

    initailized settings.py

---
+++ pakshi_resort/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for pakshi_resort project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pakshi_resort.settings')
+
+application = get_asgi_application()
