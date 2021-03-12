3bc3d32544903be9effd016a55e2d8d5e637c63c
MizanurRahmann <mr926560@gmail.com>
2021-03-13 01:52:27 +0600

    UPDATE: guest table status style.

--- frontend/src/components/Guests/Guest.js
+++ frontend/src/components/Guests/Guest.js
@@ -7,7 +7,14 @@ function Guest({name, phone, checkin, checkout, status, room}) {
             <div className="phone">{phone}</div>
             <div className="checkin">{checkin}</div>
             <div className="checkout">{checkout}</div>
-            <div className={status === "Paid" ? "status paid" : "status due"}>{status}</div>
+            <div className={status === "Paid" ? "status paid" : "status due"}>
+                {
+                    status === "Paid"
+                    ? (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" className="feather feather-check-circle"><path d="M22 11.08V12a10 10 0 1 1-5.93-9.14"></path><polyline points="22 4 12 14.01 9 11.01"></polyline></svg>)
+                    : (<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" className="feather feather-x-circle"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg>)
+                }
+                {status}
+            </div>
             <div className="room">{room}</div>
         </div>
     )
