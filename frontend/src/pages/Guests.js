ec63b31bfcb945b9261cefcaf2dc8461e8bc4002
MizanurRahmann <mr926560@gmail.com>
2021-03-13 00:29:14 +0600

    ADD: guest table.

--- frontend/src/pages/Guests.js
+++ frontend/src/pages/Guests.js
@@ -34,7 +34,7 @@ function Guests() {
         {/* HEADING */}
         <div className="heading">
           <h1>Booking</h1>
-          <div className="search-area">
+          <form className="search-area">
             <div className="field">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
@@ -51,41 +51,72 @@ function Guests() {
                 <circle cx="11" cy="11" r="8"></circle>
                 <line x1="21" y1="21" x2="16.65" y2="16.65"></line>
               </svg>
-              <input type="text" placeholder="Search" />
+              <input type="text" placeholder="Search by" />
             </div>
             <select name="Filter" id="filter">
               <option value="all">Name</option>
-              <option value="checkin">Check in</option>
-              <option value="checkout">Checkout</option>
+              <option value="phone">Phone</option>
+              <option value="room">Room no</option>
             </select>
-          </div>
+          </form>
         </div>
 
         {/* GUEST TABLE */}
         <div className="content">
-        <div className="guest-table">
-          <div className="table-heading">
-            <div className="name">Name {rsvg}</div>
-            <div className="phone">Phone {rsvg}</div>
-            <div className="checkin">Check in {rsvg}</div>
-            <div className="checkout">Check out {rsvg}</div>
-            <div className="status">Status {rsvg}</div>
-            <div className="room">Room {rsvg}</div>
-          </div>
+          <div className="guest-table">
+            <div className="table-heading">
+              <div className="name">Name {rsvg}</div>
+              <div className="phone">Phone {rsvg}</div>
+              <div className="checkin">Check in {rsvg}</div>
+              <div className="checkout">Check out {rsvg}</div>
+              <div className="status">Status {rsvg}</div>
+              <div className="room">Room {rsvg}</div>
+            </div>
 
-          {/* GUESTS */}
-          {guestsList.map((guest) => (
-            <Guest
-              name={guest.name}
-              phone={guest.phone}
-              checkin={guest.checkin}
-              checkout={guest.checkout}
-              status={guest.status}
-              room={guest.room}
-            />
-          ))}
-        </div>
-        <div className="right-nav"></div>
+            {/* GUESTS */}
+            <div className="guestentries">
+              {guestsList.map((guest) => (
+                <Guest
+                  name={guest.name}
+                  phone={guest.phone}
+                  checkin={guest.checkin}
+                  checkout={guest.checkout}
+                  status={guest.status}
+                  room={guest.room}
+                />
+              ))}
+            </div>
+          </div>
+          <div className="right-nav">
+            <form className="content">
+              <h2>Sort by</h2>
+              <div>
+                <input
+                  class="styled-checkbox"
+                  id="checkin"
+                  type="checkbox"
+                  value="checkin"
+                />
+                <label for="checkin">Check in</label>
+              </div>
+              <div>
+                <input
+                  class="styled-checkbox"
+                  id="checkout"
+                  type="checkbox"
+                  value="checkout"
+                />
+                <label for="checkout">Check out</label>
+              </div>
+            </form>
+            {/* style */}
+            <div className="right-nav-style">
+              <div className="img"></div>
+              <div className="circle big"></div>
+              <div className="circle mid"></div>
+              <div className="circle small"></div>
+            </div>
+          </div>
         </div>
       </div>
     </ContentBox>
