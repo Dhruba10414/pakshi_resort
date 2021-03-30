6891f44a0094f88b36f0f1f48886c048263c3709
MizanurRahmann <mr926560@gmail.com>
2021-03-30 12:37:20 +0600

    ADD: search available rooms func.

--- frontend/src/pages/Book.js
+++ frontend/src/pages/Book.js
@@ -2,18 +2,43 @@ import React, { useEffect, useState } from "react";
 import SceduleSetup from "../components/Booking/SceduleSetup";
 import RoomInfo from "../components/Booking/RoomInfo";
 import ContentBox from "../components/StaffSection/ContentBox";
+import axios from "axios";
 
 function Book() {
-  const [schedule, setSchedule] = useState(null);
+  const [ availableRooms, setAvailableRooms ] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState("");
 
   const updateSchedule = (startDate, endDate) => {
-    setSchedule({ startDate, endDate });
+    setLoading(true);
+    
+    const sd = startDate.getDate()
+    const sm = (startDate.getMonth() + 1).toString().padStart(2, '0');;
+    const sy = startDate.getFullYear();
+
+    const ed = endDate.getDate();
+    const em = (endDate.getMonth() + 1).toString().padStart(2, '0');
+    const ey = endDate.getFullYear();
+
+
+    // URLS
+    const REFRESH_TOKEN = localStorage.getItem("refresh_token");
+    const GET_ACCESS_TOKEN_URL = `http://127.0.0.1:8000/api/token/refresh/`;
+    const ROOM_SEARCH_URL = `http://127.0.0.1:8000/bookings/rooms/available/?check_in=${sd}-${sm}-${sy}&check_out=${ed}-${em}-${ey}&as_group=true`;
+
+    // GET DATA USING API URL
+    axios.post(GET_ACCESS_TOKEN_URL, {refresh: REFRESH_TOKEN})
+    .then((token) => {
+      const Config = {headers: { Authorization: "Bearer " + token.data.access }};
+
+      axios.get(ROOM_SEARCH_URL, Config)
+      .then((res) => { setAvailableRooms(res.data); setLoading(false); })
+      .catch((err) => { setError("Something went wrong! Try again."); setLoading(false); });
+
+    })
+    .catch((err) => { setError("Atherization Error! Please login and try again"); setLoading(false);;})
   };
 
-  // Testing purpose (should be deleted) <--
-  useEffect(() => {
-    console.log(schedule);
-  }, [schedule]);
 
   return (
     <ContentBox heading="Booking">
