6c9b7099b9a6f29d997ec894e22ff8c2ea925b28
Sudipto <sudipto00003@gmail.com>
2021-04-26 15:36:56 +0600

    food csv null guest name as anonymous

--- food/views.py
+++ food/views.py
@@ -160,8 +160,8 @@ class FoodLogView(generics.GenericAPIView):
         
         writer.writerow(['Guest', 'Guest Email', 'Order Time', 'Food Name', 'Type', 'Price', 'Quantity', 'Bill', 'Registed By'])
         for q in filtered:
-            row = [q.guest.name,
-                    q.guest.email,
+            row = [q.guest.name if q.guest else "Anonymous",
+                    q.guest.email if q.guest else "-",
                     datetime.strftime(timezone.localtime(q.time), "%d-%m-%Y %I:%M %p"),
                     q.food.name,
                     q.food.food_type,
