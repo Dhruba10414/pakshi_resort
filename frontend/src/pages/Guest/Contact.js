d935b57b14f3a481e6b9c2c6b4704649d9871c26
MizanurRahmann <mr926560@gmail.com>
2021-05-02 10:36:52 +0600

    ADD: get direction link to contact page.

--- frontend/src/pages/Guest/Contact.js
+++ frontend/src/pages/Guest/Contact.js
@@ -35,6 +35,19 @@ function Contact() {
               <p>F:880 1730706252</p>
               <p>F:880 1730706257</p>
             </div>
+            <div className="texts__phone">
+              <h3>Get direction:</h3>
+              <p>
+                Just turn on location in your device and{" "}
+                <a
+                  href="https://www.google.com/maps/dir/24.8435584,89.3701066/24.086299,89.0428347/@24.458018,88.9491725,10z/data=!3m1!4b1!4m4!4m3!1m1!4e1!1m0"
+                  target="_blank"
+                >
+                  click here
+                </a>{" "}
+                to see the best route from your home to our place.
+              </p>
+            </div>
           </div>
           <div className="image">
             {/* <img src={drone} alt="" /> */}
